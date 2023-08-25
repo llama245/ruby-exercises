a6bf7a7d86c6ad871df9e877c61a5d09d8ce8064
llama245 <lorenbap77@outlook.com>
2023-08-25 00:01:47 -0400

    finished excercises

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -1,5 +1,6 @@
 def blank_seating_chart(number_of_rows, seats_per_row)
   # return a 2d array to represent a seating chart that contains
+  Array.new(number_of_rows) {Array.new(seats_per_row)}
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
   # represent that each seat is empty.
 
@@ -15,34 +16,45 @@ end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
+  chart[row_index].push(seat_to_add)
+  return chart
   # at row_index index of the chart, then return the chart
 end
 
 def add_another_row(chart, row_to_add)
-  # take a chart and add row_to_add to the end of the chart,
+  # take a chart and add row_to_add to the end of the chart
+  chart.push(row_to_add)
   # then return the chart.
+  return chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
   # take a chart and delete the seat at seat_index of the row at row_index of
+  chart[row_index].delete_at(seat_index)
   # the chart, then return the chart
-
+  return chart
   # Hint: explore the ruby docs to find a method for deleting from an array!
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
+  chart.delete_at(row_index)
   # then return the chart
+  return chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
 
+  chart.sum { |seat| seat.count(nil)}
   # NOTE: `chart` should **not** be mutated
 end
 
 def find_favorite(array_of_hash_objects)
   # take an array_of_hash_objects and return the hash which has the key/value
+  output = nil 
+  array_of_hash_objects.each { |hash| hash[:is_my_favorite?]? output = hash : nil}
+  output
   # pair :is_my_favorite? => true. If no hash returns the value true to the key
   # :is_my_favorite? it should return nil
 
